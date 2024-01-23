f01f50251e1767db8fa93b13ef61942ecebfec93
ypbox <ypbox@yandex.ru>
2024-01-23 16:47:51 +0300

    push transform

---
+++ 1/transform.js
@@ -0,0 +1,1 @@
+{"header":{"input":{"format":"text"},"query":{"format":"text"},"prompt":{"format":"text","value":""},"result":{"format":"text"}},"rows":[{"input":"1","result":"res: { \"1\" }"},{"input":"2"}]}
