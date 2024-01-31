04663ba270a25da9629d2b9c0e1bb02a450024fa
ypbox <ypbox@yandex.ru>
2024-01-31 13:52:02 +0300

    Rename transform.js to transform.transform.js

--- 1/transform.js
+++
@@ -1,1 +0,0 @@
-{"header":{"input":{"format":"text"},"query":{"format":"text"},"prompt":{"format":"text","value":""},"result":{"format":"text"}},"rows":[{"input":"1","result":"res: { \"1\" }"},{"input":"2"}]}
